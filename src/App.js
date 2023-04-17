fc979df5d98b1dedde93ea206566f76410221639
t9fiction <sohail.sohailishaq@gmail.com>
2023-04-18 02:42:31 +0500

    roadmap bg added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Offers from "./components/Offers";
 import Working from "./components/Working";
-
+import Roadmap from "./components/Roadmap";
 function App() {
   return (
     <div className="">
@@ -12,6 +12,7 @@ function App() {
       <Hero />
       <Offers />
       <Working />
+      <Roadmap />
       <Docs />
     </div>
   );
---
+++ src/components/Roadmap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Roadmap = () => {
+  return (
+    <div className='relative h-screen'>
+        <img alt='universe' src='universe.png' className='-z-10 absolute right-0 left-0' />
+        <img alt='universe' src='shapes.png' className='absolute right-0 left-0' />
+    </div>
+  )
+}
+
+export default Roadmap
