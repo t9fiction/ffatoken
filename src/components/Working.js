40c22dd4c484619ec8e5d6238cbcd958f1e6f4d5
t9fiction <sohail.sohailishaq@gmail.com>
2023-04-28 02:23:44 +0500

    working complete

--- src/components/Working.js
+++ src/components/Working.js
@@ -3,24 +3,24 @@ import React from "react";
 const Working = () => {
   return (
     <div className="bg-white">
-      <div className="px-12 relative">
-        <div className="flex flex-row justify-center gap-x-14 bottom-20 relative items-center mx-28">
-          <div className="w-2/5 justify-center relative mx-12">
+      <div className="px-10 relative">
+        <div className="flex flex-col-reverse md:flex-row justify-center md:gap-x-14 md:-top-20 relative items-center md:mx-28">
+          <div className="md:w-2/5 justify-center -top-4 relative md:mx-12">
             <img
               alt=""
               src="maskgroup.png"
-              className="w-[320px] h-[360px] relative top-32 border-4 border-white right-0 left-24"
+              className="w-[320px] h-[360px] relative top-12 left-16 md:top-32 border-4 border-white md:right-0 md:left-24"
             />
             <img
               alt=""
               src="image8.png"
-              className="w-[320px] border-4 border-white h-[360px] relative right-28 left-0 bottom-16"
+              className="w-[320px] border-4 border-white h-[360px] relative right-9 bottom-24 md:right-28 md:left-0 md:bottom-16"
             />
           </div>
-          <div className="flex flex-col w-3/5 space-y-4 mt-20">
-            <p className="uppercase text-primary">more details</p>
-            <h1 className="font-aokupro text-3xl">How does it work?</h1>
-            <p className="text-[16px] pt-6">
+          <div className="flex flex-col md:w-3/5 w-full space-y-4 mt-20">
+            <p className="uppercase text-primary text-center md:text-left">more details</p>
+            <h1 className="font-aokupro text-2xl text-center md:text-left md:text-3xl">How does it work?</h1>
+            <p className="text-[16px] pt-2 sm:pt-4 md:pt-5 text-justify md:text-left">
               In our agricultural projects, we follow a careful process to
               identify the most suitable lands for farming. Our specialists
               conduct complete research and analysis to determine which crop
