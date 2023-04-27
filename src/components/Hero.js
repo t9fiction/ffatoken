a4585a21e1e6a8ebed57122378ccd3fe8b7720f7
t9fiction <sohail.sohailishaq@gmail.com>
2023-04-28 01:17:49 +0500

    video zoom done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative h-[228px] sm:h-[424px] md:h-screen overflow-hidden -z-10 border-primary border-b-8">
-      <video className="absolute -z-10 sm:w-full md:min-h-full" autoPlay loop muted>
+    <div className="relative h-[430px] sm:h-[424px] md:h-screen overflow-hidden -z-10 border-primary border-b-8">
+      <video className="absolute -z-10 sm:w-full md:min-h-full mobile-video" autoPlay loop muted>
         <source src="/treepro.mp4" type="video/mp4" />
         {/* <source
           src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
           type="video/mp4"
         /> */}
       </video>
-      <div className="relative left-0 right-0 md:bottom-[24px] top-[106px] sm:top-60 md:top-72 md:w-[55%] mx-auto justify-center items-center">
+      <div className="relative left-0 right-0 md:bottom-[24px] top-72 sm:top-60 md:top-72 md:w-[55%] mx-auto justify-center items-center">
         <h1 className="text-center text-white text-[20px] sm:text-[40px] md:text-[76px] uppercase mx-auto font-aokupro">
           Agritokenization
         </h1>
@@ -18,7 +18,7 @@ const Hero = () => {
           <span className="text-primary"> & </span>
           Sustainability
         </h1>
-        <p className="text-center text-white text-[11px] sm:text-[16px] md:text-[20px] md:pt-4 pt-2 px-[88px] sm:px-44 md:px-20">
+        <p className="text-center text-white text-[11px] sm:text-[16px] md:text-[20px] md:pt-4 pt-3 px-[88px] sm:px-44 md:px-20">
           Connecting investors with farmers and improving the needs of both
           through the use of Tokenization
         </p>
