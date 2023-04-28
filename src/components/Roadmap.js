3f66c0cce28c60096fce9f1269ecba2e1916f723
t9fiction <sohail.sohailishaq@gmail.com>
2023-04-28 17:33:41 +0500

    mobile timeline working

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -2,14 +2,15 @@ import React from "react";
 import Design from "./Design";
 import Timeline2 from "./Timeline2";
 import Skyanimation from "./Skyanimation";
+import TimelineMob from "./TimelineMob";
 
 const Roadmap = () => {
   return (
-    <div className="relative h-[1000px] ">
+    <div className="relative md:h-[1000px] ">
       <img
         alt="universe"
         src="universe.png"
-        className="-z-30 absolute right-0 left-0"
+        className="-z-30 absolute right-0 left-0 h-full"
       />
       {/* <img
         alt="universe"
@@ -17,13 +18,13 @@ const Roadmap = () => {
         className="absolute right-0 left-0 -z-10"
       /> */}
       <Skyanimation />
-      <div className="py-16  mx-auto flex flex-col">
-        <div className="text-center px-[25%]">
+      <div className="py-16  mx-auto flex flex-col min-h-full">
+        <div className="text-center px-10 md:px-[25%]">
           <h3 className="text-primary uppercase">our journey</h3>
           <h2 className="text-4xl  py-4 font-semibold font-aokupro tracking-widest text-white">
             Roadmap
           </h2>
-          <p className="py-5 text-white">
+          <p className="py-5 text-white text-justify md:text-center">
             Here is the roadmap for bringing our idea to life! We have carefully
             planned out the steps necessary to make it happen and are eager to
             take action. With your support and dedication, we believe we can
@@ -32,8 +33,13 @@ const Roadmap = () => {
             on this journey.
           </p>
         </div>
-        {/* <Timeline /> */}
+        <div className="hidden md:block">
         <Timeline2 />
+        </div>
+        <div className="md:hidden">
+          <TimelineMob />
+        </div>
+        {/* <Timeline /> */}
       </div>
       <div className="-z-20 relative">
         <Design />
---
+++ src/components/TimelineMob.js
@@ -0,0 +1,268 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { useState } from "react";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+const TimelineMob = () => {
+  const [display, setDisplay] = useState(false);
+
+  const handleDisplay = () => {
+    setDisplay((prev) => !prev);
+  };
+
+  return (
+    <section class="items-center">
+      <div class="justify-center w-full px-4 py-4  ">
+        {/* first event */}
+        <div className="flex flex-col group hover:cursor-pointer">
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div class="w-px h-6 bg-primary"></div>
+            </div>
+            <div>
+              <h2 class="px-0 text-base text-primary justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                Phase - I <span className="text-white pl-2">Q2 2023</span>
+              </h2>
+            </div>
+          </div>
+
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div className="">
+                <div class="flex items-center justify-center w-4 h-4 group-hover:w-6 ease-in-out duration-300 group-hover:h-6 bg-primary rounded-full">
+                  <div class="group-hover:w-4 group-hover:h-4 w-3 h-3 ease-in-out duration-300 bg-secondary group-hover:bg-white rounded-full group-hover:border-secondary group-hover:border-2"></div>
+                </div>
+              </div>
+              <div class="w-px h-16 bg-primary"></div>
+            </div>
+            <div>
+              <h2 class="px-0  text-base text-primary justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                Pre-Launch and Token Distribution
+              </h2>
+            </div>
+          </div>
+        </div>
+
+        {/* Second */}
+        <div className="flex flex-col group hover:cursor-pointer">
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div class="w-px h-6 bg-primary"></div>
+            </div>
+            <div>
+              <h2 class="px-0 text-base text-white justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                April 2023
+              </h2>
+            </div>
+          </div>
+
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div className="items-start flex">
+                <div class="flex items-center ease-in-out duration-300 justify-center w-4 h-4 border-2 group-hover:w-6 group-hover:h-6 border-transparent  bg-primary rounded-full">
+                  <div class="w-2 h-2 group-hover:w-4 group-hover:h-4 ease-in-out duration-300 bg-secondary group-hover:border-secondary group-hover:border-2 group-hover:bg-white rounded-full"></div>
+                </div>
+              </div>
+              <div class="w-px h-16 bg-primary"></div>
+            </div>
+            <div>
+              <p class="px-0 max-w-[300px] sm:max-w-lg  text-sm text-white justify-start ease-in-out duration-300  group-hover:text-primary">
+                Finalize the FFA smart contract code and have it audited by a
+                reputable third-party auditing firm.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Third */}
+        <div className="flex flex-col group hover:cursor-pointer">
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div class="w-px h-6 bg-primary"></div>
+            </div>
+            <div>
+              <h2 class="px-0 text-base text-white justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                May 2023
+              </h2>
+            </div>
+          </div>
+
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div className="items-start flex">
+                <div class="flex items-center ease-in-out duration-300 justify-center w-4 h-4 border-2 group-hover:w-6 group-hover:h-6 border-transparent  bg-primary rounded-full">
+                  <div class="w-2 h-2 group-hover:w-4 group-hover:h-4 ease-in-out duration-300 bg-secondary group-hover:border-secondary group-hover:border-2 group-hover:bg-white rounded-full"></div>
+                </div>
+              </div>
+              <div class="w-px h-16 bg-primary"></div>
+            </div>
+            <div>
+              <p class="px-0 max-w-[300px] sm:max-w-lg  text-sm text-white justify-start ease-in-out duration-300  group-hover:text-primary">
+                Launch the FFA website, including the whitepaper, token
+                distribution plan, team information, and contact information.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Event */}
+        <div className="flex flex-col group hover:cursor-pointer">
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div class="w-px h-6 bg-primary"></div>
+            </div>
+            <div>
+              <h2 class="px-0 text-base text-white justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                June 2023
+              </h2>
+            </div>
+          </div>
+
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+              <div className="items-start flex">
+                <div class="flex items-center ease-in-out duration-300 justify-center w-4 h-4 border-2 group-hover:w-6 group-hover:h-6 border-transparent  bg-primary rounded-full">
+                  <div class="w-2 h-2 group-hover:w-4 group-hover:h-4 ease-in-out duration-300 bg-secondary group-hover:border-secondary group-hover:border-2 group-hover:bg-white rounded-full"></div>
+                </div>
+              </div>
+              <div class="w-px h-16 bg-primary"></div>
+            </div>
+            <div>
+              <p class="px-0 max-w-[300px] sm:max-w-lg  text-sm text-white justify-start ease-in-out duration-300  group-hover:text-primary">
+                Distribute FFA tokens to the Liquidity Pool, Incentives,
+                Treasury Shares, and Team, as outlined in the token distribution
+                plan.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {display && (
+          <>
+            <div className="flex flex-col group hover:cursor-pointer">
+              <div class="relative flex justify-start">
+                <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+                  <div class="w-px h-6 bg-primary"></div>
+                </div>
+                <div>
+                  <h2 class="px-0 text-base text-white justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                    July 2023
+                  </h2>
+                </div>
+              </div>
+
+              <div class="relative flex justify-start">
+                <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+                  <div className="items-start flex">
+                    <div class="flex items-center ease-in-out duration-300 justify-center w-4 h-4 border-2 group-hover:w-6 group-hover:h-6 border-transparent  bg-primary rounded-full">
+                      <div class="w-2 h-2 group-hover:w-4 group-hover:h-4 ease-in-out duration-300 bg-secondary group-hover:border-secondary group-hover:border-2 group-hover:bg-white rounded-full"></div>
+                    </div>
+                  </div>
+                  <div class="w-px h-16 bg-primary"></div>
+                </div>
+                <div>
+                  <p class="px-0 max-w-[300px] sm:max-w-lg  text-sm text-white justify-start ease-in-out duration-300  group-hover:text-primary">
+                    Distribute FFA tokens to the Liquidity Pool, Incentives,
+                    Treasury Shares, and Team, as outlined in the token
+                    distribution plan.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col group hover:cursor-pointer">
+              <div class="relative flex justify-start">
+                <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+                  <div class="w-px h-6 bg-primary"></div>
+                </div>
+                <div>
+                  <h2 class="px-0 text-base text-white justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                    August 2023
+                  </h2>
+                </div>
+              </div>
+
+              <div class="relative flex justify-start">
+                <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+                  <div className="items-start flex">
+                    <div class="flex items-center ease-in-out duration-300 justify-center w-4 h-4 border-2 group-hover:w-6 group-hover:h-6 border-transparent  bg-primary rounded-full">
+                      <div class="w-2 h-2 group-hover:w-4 group-hover:h-4 ease-in-out duration-300 bg-secondary group-hover:border-secondary group-hover:border-2 group-hover:bg-white rounded-full"></div>
+                    </div>
+                  </div>
+                  <div class="w-px h-16 bg-primary"></div>
+                </div>
+                <div>
+                  <p class="px-0 max-w-[300px] sm:max-w-lg  text-sm text-white justify-start ease-in-out duration-300  group-hover:text-primary">
+                    Distribute FFA tokens to the Liquidity Pool, Incentives,
+                    Treasury Shares, and Team, as outlined in the token
+                    distribution plan.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col group hover:cursor-pointer">
+              <div class="relative flex justify-start">
+                <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+                  <div class="w-px h-6 bg-primary"></div>
+                </div>
+                <div>
+                  <h2 class="px-0 text-base text-white justify-start ease-in-out duration-300 group-hover:font-bold font-medium group-hover:text-white">
+                    September 2023
+                  </h2>
+                </div>
+              </div>
+
+              <div class="relative flex justify-start">
+                <div class="flex flex-col items-center w-10 mr-3 md:w-24">
+                  <div className="items-start flex">
+                    <div class="flex items-center ease-in-out duration-300 justify-center w-4 h-4 border-2 group-hover:w-6 group-hover:h-6 border-transparent  bg-primary rounded-full">
+                      <div class="w-2 h-2 group-hover:w-4 group-hover:h-4 ease-in-out duration-300 bg-secondary group-hover:border-secondary group-hover:border-2 group-hover:bg-white rounded-full"></div>
+                    </div>
+                  </div>
+                  <div class="w-px h-16 bg-primary"></div>
+                </div>
+                <div>
+                  <p class="px-0 max-w-[300px] sm:max-w-lg  text-sm text-white justify-start ease-in-out duration-300  group-hover:text-primary">
+                    Distribute FFA tokens to the Liquidity Pool, Incentives,
+                    Treasury Shares, and Team, as outlined in the token
+                    distribution plan.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+        {/* Last */}
+        <div className="flex flex-col group hover:cursor-pointer">
+          <div class="relative flex justify-start">
+            <div class="flex flex-col items-center w-10 mr-4 md:w-24">
+              <div className="">
+                <div class="flex items-center justify-center w-6 h-6 border-2 border-transparent ease-in-out duration-300 bg-primary group-hover:bg-white rounded-full">
+                  <div class="w-4 h-4 bg-white group-hover:bg-primary rounded-full group-hover:w-3 group-hover:h-3 group-hover:border-secondary ease-in-out duration-300 items-center justify-center flex border-2">
+                    <button onClick={handleDisplay}>
+                      {display ? (
+                        <MdKeyboardArrowUp />
+                      ) : (
+                        <MdKeyboardArrowDown />
+                      )}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p class="group-hover:text-white ease-in-out duration-300 px-0  text-sm  text-primary justify-start underline">
+                {display ? <a href="#">See Less</a> : <a href="#">See All</a>}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Ending */}
+      </div>
+    </section>
+  );
+};
+
+export default TimelineMob;
