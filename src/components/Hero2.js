e4562a36e5049455eb5d853b5c2fdce347f9cbd4
t9fiction <sohail.sohailishaq@gmail.com>
2023-05-03 03:04:20 +0500

    working tree

--- src/components/Hero2.js
+++ src/components/Hero2.js
@@ -22,10 +22,23 @@ const Hero2 = () => {
         playsInline
       >
         <source
-          src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
+          src="twittertree.mp4"
           type="video/mp4"
         />
       </video>
+      <div className="relative left-0 right-0 md:bottom-[24px] top-72 sm:top-60 md:top-72 md:w-[55%] mx-auto justify-center items-center">
+        <h1 className="text-center text-white text-[20px] sm:text-[40px] md:text-6xl uppercase mx-auto font-aokupro">
+          Agritokenization
+        </h1>
+        <h1 className="text-center text-white text-[20px] sm:text-[40px] md:text-6xl leading-3 md:leading-tight uppercase font-aokupro">
+          <span className="text-primary"> & </span>
+          Sustainability
+        </h1>
+        <p className="text-center text-white text-[11px] sm:text-[16px] md:text-[20px] md:pt-4 pt-3 px-[88px] sm:px-44 md:px-20">
+          Connecting investors with farmers and improving the needs of both
+          through the use of Tokenization
+        </p>
+      </div>
     </div>
   );
 };
