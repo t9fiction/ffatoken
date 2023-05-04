9543b2ede89df59947b0e37604f67d7cbbb2d2f2
t9fiction <sohail.sohailishaq@gmail.com>
2023-05-04 16:34:01 +0500

    data reconfigured her

--- src/components/Hero2.js
+++ src/components/Hero2.js
@@ -26,15 +26,15 @@ const Hero2 = () => {
           type="video/mp4"
         />
       </video>
-      <div className="relative left-0 right-0 md:bottom-[24px] top-72 sm:top-60 md:top-72 md:w-[55%] mx-auto justify-center items-center">
-        <h1 className="text-center text-white text-[20px] sm:text-[40px] md:text-6xl uppercase mx-auto font-aokupro">
+      <div className="relative left-0 right-0 md:bottom-[24px] top-[70%] sm:top-[55%] md:top-[60%] md:w-[55%] mx-auto justify-center items-center text-center">
+        <h1 className="text-center text-white text-[20px] sm:text-[40px] md:text-6xl uppercase mx-auto font-aokupro justify-center items-center flex">
           Agritokenization
         </h1>
-        <h1 className="text-center text-white text-[20px] sm:text-[40px] md:text-6xl leading-3 md:leading-tight uppercase font-aokupro">
-          <span className="text-primary"> & </span>
+        <h1 className="text-center text-white text-[20px] sm:text-[40px] md:text-6xl leading-2 md:leading-tight uppercase font-aokupro">
+          <span className="text-primary">& </span>
           Sustainability
         </h1>
-        <p className="text-center text-white text-[11px] sm:text-[16px] md:text-[20px] md:pt-4 pt-3 px-[88px] sm:px-44 md:px-20">
+        <p className="text-center text-white text-[11px] sm:text-[16px]  md:text-[20px] md:pt-2 pt-1 px-[10%] sm:px[10%] md:px-20">
           Connecting investors with farmers and improving the needs of both
           through the use of Tokenization
         </p>
